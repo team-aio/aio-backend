df6d7d8da98b3c07228a580afc7bfe1805aada1b
howudong <tjdvy963@naver.com>
2024-12-29 21:10:48 +0900

    Feat: Spring Security 의존성 추가 및 CORS 관련 설정 Security 형식으로 변경 + 기본적인 Security 설정

--- src/main/kotlin/team/aio/config/SecurityConfig.kt
+++ src/main/kotlin/team/aio/config/SecurityConfig.kt
@@ -2,21 +2,48 @@ package team.aio.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig {
-    // TODO : CORS에 대한 처리를 Spring Security를 활용하는 방식으로 변경해야함
     @Bean
-    fun corsConfigurer() = object : WebMvcConfigurer {
-        override fun addCorsMappings(registry: CorsRegistry) {
-            registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000") // 특정 Origin만 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true) // 인증 정보 허용 (쿠키 등)
-                .maxAge(3600) // preflight 요청 캐시 시간 (초)
+    fun corsConfigSource(): CorsConfigurationSource {
+        val corsConfig = CorsConfiguration()
+
+        corsConfig.apply {
+            allowedHeaders = listOf("*")
+            allowedOrigins = listOf("http://localhost:3000")
+            exposedHeaders = listOf("*")
+            allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            allowCredentials = true
+            maxAge = 3600
         }
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", corsConfig)
+        return source
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .cors { it.configurationSource(corsConfigSource()) } // cors 설정 활성화
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+
+        http.authorizeHttpRequests {
+            it.requestMatchers("/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        }
+
+        return http.build()
     }
 }
