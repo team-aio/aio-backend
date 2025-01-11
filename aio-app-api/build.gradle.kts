72126f7de55356724f87fa372223a429f77f48c5
howudong <tjdvy963@naver.com>
2025-01-11 11:46:00 +0900

    Feat: aio-core-web 모듈로 스프링 시큐리티 관련 코드 이동

--- aio-app-api/build.gradle.kts
+++ aio-app-api/build.gradle.kts
@@ -5,12 +5,11 @@ bootJar.enabled = true
 
 dependencies {
     implementation(project(":aio-display-swagger"))
+    implementation(project(":aio-core-web"))
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    testImplementation("org.springframework.security:spring-security-test")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
