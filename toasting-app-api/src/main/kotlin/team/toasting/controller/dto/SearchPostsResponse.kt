3468b8ef6a9281c7179da49b4142fc4d41c8204d
preferrrr <zzangsh2946@naver.com>
2025-01-28 14:15:22 +0900

    feat : 게시글 검색 mock api 추가

---
+++ toasting-app-api/src/main/kotlin/team/toasting/controller/dto/SearchPostsResponse.kt
@@ -0,0 +1,28 @@
+package team.toasting.controller.dto
+
+import java.time.LocalDateTime
+
+data class SearchPostsResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val likeCount: Int,
+    val postedAt: LocalDateTime ?,
+    val sourceType: String,
+    val memberInfo: MemberInfo
+) {
+    companion object {
+        fun mock() = SearchPostsResponse(
+            id = 1L,
+            title = "mock title",
+            content = "mock short content mock short content mock short content mock short content mock short content " +
+                    "mock short content mock short content mock short content mock short content mock short content " +
+                    "mock short content mock short content mock short content mock short content mock short content " +
+                    "mock short content mock short content mock short content mock short content mock short content mock short content",
+            likeCount = 100,
+            postedAt = LocalDateTime.now(),
+            sourceType = "velog",
+            memberInfo = MemberInfo.mock()
+        )
+    }
+}
