c85d2dffc67b460ec7319d1c55129e2a69bb9c4a
preferrrr <zzangsh2946@naver.com>
2025-01-28 20:36:20 +0900

    feat : 메세지 전송 mock api 추가

--- toasting-app-api/src/main/kotlin/team/toasting/controller/MessageController.kt
+++ toasting-app-api/src/main/kotlin/team/toasting/controller/MessageController.kt
@@ -1,10 +1,12 @@
 package team.toasting.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import team.toasting.api.ApiResponse
 import team.toasting.controller.dto.GetMessageCountResponse
+import team.toasting.controller.dto.SendMessageResponse
 
 @RestController
 @RequestMapping("/v1/messages")
@@ -13,5 +15,10 @@ class MessageController {
     @GetMapping("/count")
     fun getMessageCount(): ApiResponse<GetMessageCountResponse> {
         return ApiResponse.onSuccess(GetMessageCountResponse.mock())
+    }
+
+    @PostMapping
+    fun sendMessage(): ApiResponse<SendMessageResponse> {
+        return ApiResponse.onSuccess(SendMessageResponse.mock())
     }
 }
---
+++ toasting-app-api/src/main/kotlin/team/toasting/controller/dto/SendMessageResponse.kt
@@ -0,0 +1,21 @@
+package team.toasting.controller.dto
+
+import java.time.LocalDateTime
+
+class SendMessageResponse (
+    val id: Long,
+    val postId: Long ?,
+    val receiverId: Long,
+    val content: String,
+    val createdAt: LocalDateTime
+) {
+    companion object {
+        fun mock() = SendMessageResponse(
+            id = 1L,
+            postId = 2L,
+            receiverId = 3L,
+            content = "Hello World",
+            createdAt = LocalDateTime.now()
+        )
+    }
+}
