ac8e6fd65c404e73af876da3ed931bcb525a569b
preferrrr <zzangsh2946@naver.com>
2025-01-28 21:34:44 +0900

    fix : data-jpa implement -> api

--- toasting-domain-jpa/build.gradle.kts
+++ toasting-domain-jpa/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 }
 
 dependencies {
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
