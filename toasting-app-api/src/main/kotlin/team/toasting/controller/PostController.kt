f9ea49dee27d14ea195f6ef8e0ef2548968b6c7b
preferrrr <zzangsh2946@naver.com>
2025-01-29 16:05:31 +0900

    feat : search post PageDefault 추가

--- toasting-app-api/src/main/kotlin/team/toasting/controller/PostController.kt
+++ toasting-app-api/src/main/kotlin/team/toasting/controller/PostController.kt
@@ -1,5 +1,8 @@
 package team.toasting.controller
 
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,7 +14,9 @@ import team.toasting.controller.dto.response.*
 internal class PostController {
 
     @GetMapping("/search")
-    fun searchPosts(): ApiResponse<SearchPostsResponse> {
+    fun searchPosts(
+        @PageableDefault(page = 0, size = 10, sort = arrayOf("postedAt"), direction = Sort.Direction.DESC) pageable: Pageable
+    ): ApiResponse<SearchPostsResponse> {
         return ApiResponse.onSuccess(SearchPostsResponse.mock())
     }
 }
