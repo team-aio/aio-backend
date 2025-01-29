fae02e637d416eab4739e16cb9fe9475c828a129
preferrrr <zzangsh2946@naver.com>
2025-01-29 13:48:27 +0900

    feat : build.gradle validation dependencies 추가

--- toasting-app-api/build.gradle.kts
+++ toasting-app-api/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(project(":toasting-core-api-response"))
     implementation(project(":toasting-domain-jpa"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
